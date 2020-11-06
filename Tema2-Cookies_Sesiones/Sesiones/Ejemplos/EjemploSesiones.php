bfe24c4573165002e9a69da51c13fcb421a17722
cralr <rafamcruza10@gmail.com>
2020-11-06 10:46:52 +0100

    Ejemplo contador sesiones

--- Tema2-Cookies_Sesiones/Sesiones/Ejemplos/EjemploSesiones.php
+++ Tema2-Cookies_Sesiones/Sesiones/Ejemplos/EjemploSesiones.php
@@ -6,15 +6,6 @@
 
     //echo session_id(); muestra un id que debe ser igual en la otra página.
 
-    
-    /* if (!isset($_SESSION["mensaje"]))
-        $_SESSION["mensaje"] = "Hola mundo Script 1";
-
-    echo "Página 1</br>";
-
-    echo $_SESSION["mensaje"]."</br>"; */
-
-
     if (!isset($_SESSION["horaInicio"])){
         $_SESSION["horaInicio"] = time();
         $_SESSION["contador"]=0;
@@ -41,16 +32,8 @@
         header("Location:EjemploSesiones.php");
     } 
 
-    /* if(isset($_POST["salir"])){
-        cerrarSesion();
-    } */
 
     echo "</br><a href=\"https://github.com/cralr/DWES2021/blob/master/Tema2-Cookies_Sesiones/Sesiones/Ejemplos/EjemploSesiones.php\"><button>Ver Código</button></a></br>";
-
-    //echo "<a href=\"EjemploSesiones2.php\">Página 2</a>";
-   /*  echo "<form action=".$_SERVER['PHP_SELF']." method='post'>";
-        echo "</br><input type=\"submit\" name=\"salir\" value=\"Salir\">";
-    echo "</form>";  */
 
     
 ?>
