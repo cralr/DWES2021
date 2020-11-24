48ae7fc14b39acec6801a220adee823c00f3e010
cralr <rafamcruza10@gmail.com>
2020-11-24 09:17:07 +0100

    Estilo

--- Tema3-Ficheros/Ejemplos/formulario_ficheros.php
+++ Tema3-Ficheros/Ejemplos/formulario_ficheros.php
@@ -57,6 +57,8 @@ if (isset($_POST["enviar"])) {
     <meta charset="UTF-8">
     <meta name="author" content="Rafael Miguel Cruz Álvarez">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/PaginaInicio/css/style.css">
+    <link rel="stylesheet" href="/PaginaInicio/css/normalize.css">
     <title>Ejemplo formulario fichero</title>
 </head>
 
